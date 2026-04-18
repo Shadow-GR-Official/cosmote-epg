5327e496de0b556a95b98ff2c1448bdb398dfe44
Σκιά <skia.official.gr@gmail.com>
2026-04-18 07:28:52 +0300

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -136,9 +136,9 @@ for ch in channels:
             if isinstance(genres, list):
                 for g in genres:
                     if g:
-                        ET.SubElement(prog, "category" "el").text = str(g)
+                        ET.SubElement(prog, "category", {"lang": "el"}).text = str(g)
             else:
-                ET.SubElement(prog, "category" "el").text = str(genres)
+                ET.SubElement(prog, "category", {"lang": "el"}).text = str(genres)
 
         total += 1
 
