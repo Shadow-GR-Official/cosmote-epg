2f5769d8ddd2b3d4cd882245438decd089f5f00e
Σκιά <skia.official.gr@gmail.com>
2026-04-20 00:19:13 +0300

    Update fetch_channels.py

--- fetch_channels.py
+++ fetch_channels.py
@@ -56,7 +56,7 @@ def run():
 
     print("[FETCH] Cosmote EPG")
 
-    for i in range(0, 3):
+    for i in range(0, 7):
         day = datetime.now() + timedelta(days=i)
 
         from_ts = int(day.replace(hour=0, minute=0, second=0).timestamp())
