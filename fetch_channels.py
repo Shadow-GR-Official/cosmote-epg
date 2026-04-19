f5b2069b06f3f96880e7836a673191c8545b91c2
Σκιά <skia.official.gr@gmail.com>
2026-04-20 00:35:13 +0300

    Update fetch_channels.py

--- fetch_channels.py
+++ fetch_channels.py
@@ -11,7 +11,7 @@ os.makedirs("data", exist_ok=True)
 
 def safe_get(session, url):
     try:
-        r = session.get(url, timeout=200)
+        r = session.get(url, timeout=10)
 
         if r.status_code != 200:
             print("HTTP ERROR:", r.status_code)
